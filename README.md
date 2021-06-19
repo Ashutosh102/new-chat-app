ef2414c1c50d26dfe26b1690511ec81d6c23dd9b
Ashutosh Mohanty <ashutoshmohanty3815@gmail.com>
2021-06-19 21:17:10 +0530

    Initial one

---
+++ README.md
@@ -0,0 +1,14 @@
+# Realtime Chat Application
+
+
+
+
+## Introduction
+This is a code repository for the Chat App. 
+
+In this repo, we will create a full Realtime Chat Application. We're going to use  React on the front end, with NodeJS + Socket.io web socket library on the back end. 
+
+By the end of this video, you will have a strong understanding of how to send and receive messages using web sockets and Socket.io to make any real-time application.
+
+Setup:
+- run ```npm i && npm start``` for both client and server side to start the development server
---
+++ client/build/precache-manifest.59404e3575a3033aca2b540fd0ca9a63.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ceb1bd335b95bb07fd4b8cf2ff2455f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "19d4af23bc29583bb68b",
+    "url": "/static/css/main.bb780985.chunk.css"
+  },
+  {
+    "revision": "aa26471d1354c730a4a3",
+    "url": "/static/js/2.78ca8566.chunk.js"
+  },
+  {
+    "revision": "19d4af23bc29583bb68b",
+    "url": "/static/js/main.56f0f081.chunk.js"
+  },
+  {
+    "revision": "20dcb6334915f836950a",
+    "url": "/static/js/runtime~main.00070426.js"
+  }
+]);
